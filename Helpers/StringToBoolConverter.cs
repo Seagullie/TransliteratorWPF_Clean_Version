c670e71e581efe18f1737959cb0d87ae453ad403
Seagullie <59409233+Seagullie@users.noreply.github.com>
2023-01-27 23:32:25 +0200

    Change state label color by disabling or enabling it

---
+++ Helpers/StringToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace TransliteratorWPF_Version.Helpers
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string stringValue = value.ToString();
+
+            if (stringValue == "On")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
