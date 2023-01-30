b7181ff163a333e21f36a3ff137286c846f19124
bind-w-exit <12andriy24@gmail.com>
2023-01-30 14:46:38 +0200

    Add FileService

---
+++ Services/FileService.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TransliteratorWPF_Version.Services
+{
+    public static class FileService
+    {
+        public static string BaseDirectory { get; }
+
+        static FileService()
+        {
+            BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        public static T Read<T>(string folderPath, string fileName)
+        {
+            var path = Path.Combine(folderPath, fileName);
+            if (File.Exists(path))
+            {
+                var json = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+
+            return default;
+        }
+
+        public static void Save<T>(string folderPath, string fileName, T content)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            var fileContent = JsonConvert.SerializeObject(content);
+            File.WriteAllText(Path.Combine(folderPath, fileName), fileContent, Encoding.UTF8);
+        }
+
+        public static void Delete(string folderPath, string fileName)
+        {
+            if (fileName != null && File.Exists(Path.Combine(folderPath, fileName)))
+            {
+                File.Delete(Path.Combine(folderPath, fileName));
+            }
+        }
+
+        // TODO: Improve?
+        public static List<string> GetFiles(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return Directory.GetFiles(path).ToList();
+            }
+            else return new List<string>();
+        }
+    }
+}
